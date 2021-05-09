4d856f4b5e4dda0ff012b5bc4ac95d35919916d7
supun <supunmadushanka19980822@gmail.com>
2021-05-09 16:57:20 +0530

    test7

--- routes/api.js
+++ routes/api.js
@@ -36,10 +36,11 @@ io.on('connection', (socket) => {
 });
 
 const sqlconfig = {
-    user: 'sa',
-    password: '123',
-    server: 'DESKTOP-1CHJNQH',
-    database: 'sms',
+    db: '*sql.DB',
+    user: 'supun',
+    password: 'Ranjani1970#',
+    server: 'mysport-codefreks.database.windows.net',
+    database: 'mysport',
     port: 1433,
     "options": {
         "encrypt": true,
--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const sqlconfig = {
+    db: '*sql.DB',
+    user: 'supun',
+    password: 'Ranjani1970#',
+    server: 'mysport-codefreks.database.windows.net',
+    database: 'mysport',
+    port: 1433,
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+    }
+};
+
 app.use('/api', api)
 
 app.get('/', function(req, res) {
